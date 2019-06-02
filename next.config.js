70e78dad777644d434c4c77c6b97b4134e854bfb
Luigi Mattia <mattia.buffetti@gmail.com>
2019-06-02 23:34:55 +0100

    Fix Jest Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require("@zeit/next-sass");
+const withImages = require('next-images');
+
 
 module.exports = withTypescript(
-    withSass()
+    withImages(
+        withSass()
+    )
 );
---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+const icon = require('../assets/img/favicon.ico');
+
+class MyApp extends App {
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return { pageProps };
+    }
+    render() {
+        const { Component, pageProps } = this.props;
+
+        return (
+            <div>
+                <Head>
+                    <title>Hangman</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                    <link rel="icon" type="image/x-icon" href={icon} />
+                </Head>
+                <Component { ...pageProps } />
+            </div>
+        );
+    }
+}
+
+export default MyApp;
