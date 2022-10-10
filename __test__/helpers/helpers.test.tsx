71be9a5ccdd0eebb28e7d121d8bca9a38ea6cfc2
mattiabuffetti <Brandino.85>
2022-10-10 23:55:21 +0100

    Fix Jest Tests

--- __test__/helpers/helpers.test.tsx
+++ __test__/helpers/helpers.test.tsx
@@ -1,9 +1,9 @@
-import { randomizeWord, displayWord } from '../helpers'
+import { randomizeWord, displayFormatWord } from '../../utils/helpers'
 
 test('should return a string', async () => {
     expect(randomizeWord(['test'])).toEqual('test');
 });
 
 test('should return a string', async () => {
-    expect(displayWord('test')).toEqual(['t', '_', '_', 't']);
+    expect(displayFormatWord('test')).toEqual(['t', '_', '_', 't']);
 });
